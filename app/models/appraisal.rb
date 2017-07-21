8e96bfc0677563ab4938225a800ae713124a6d30
Steven Velez-Garcia <steven.velezgarcia@gmail.com>
2017-07-21 17:29:47 -0400

    Appraisal modal works

--- app/models/appraisal.rb
+++ app/models/appraisal.rb
@@ -1,4 +1,5 @@
 class Appraisal < ApplicationRecord
+  enum condition: [:Mint, :Good, :Average, :Damaged]
   belongs_to :item
   validates :condition, presence: true, numericality: { in: 0..3 }
 end
